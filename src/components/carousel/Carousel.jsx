7bfe16655ca69d357b8e4c11ba35dbce4c0479a9
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-21 16:31:23 +0600

    carousel component design

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,46 @@
+import banner4 from '../../assets/banner1.jpg'
+import banner3 from '../../assets/banner2.jpg'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
+
+export default function Carousel() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {/* <SwiperSlide>
+          <img src={banner1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} alt="" />
+        </SwiperSlide> */}
+        <SwiperSlide>
+          <img src={banner3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner4} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
+import Carousel from '../components/carousel/Carousel'
+
 export default function HomePage() {
-  return <></>
+  return (
+    <>
+      <Carousel />
+    </>
+  )
 }
