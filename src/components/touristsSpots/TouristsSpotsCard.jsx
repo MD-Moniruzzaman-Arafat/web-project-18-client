b327ec5f466bbbb86267f1e474d32e12319048b9
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-21 17:53:06 +0600

    tourist spots section component design

---
+++ src/components/touristsSpots/TouristsSpotsCard.jsx
@@ -0,0 +1,21 @@
+export default function TouristsSpotsCard({ img }) {
+  return (
+    <>
+      <div className="card bg-base-100 shadow-sm">
+        <figure>
+          <img src={img} alt="Shoes" className="h-50 w-full" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Carousel from '../components/carousel/Carousel'
+import TouristsSpots from '../components/touristsSpots/TouristsSpots'
 
 export default function HomePage() {
   return (
     <>
       <Carousel />
+      <TouristsSpots />
     </>
   )
 }
