f44235172e81909b19c4030767e656fea5305556
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-22 20:32:20 +0600

    footer section component design

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router'
+import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 
 export default function Root() {
@@ -6,6 +7,7 @@ export default function Root() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
